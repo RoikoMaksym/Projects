b5f46ee2b2c4e091b332a899393c834ea23d6f38
RoikoMaksym <royko.maksim@ukr.net>
2026-09-09 16:05:55 +0300

    Add files via upload
    laba 1

---
+++ script.py
@@ -0,0 +1,38 @@
+print("Hello, World!")
+name = 'Max' # змінна для символів
+number = 52.3 # змінна для цифр
+
+print(name)
+print(number)
+
+print(int(12))
+print(float(12.1))
+print(str('aba12'))
+print(bool(10))
+print(bool("a"))
+print(list[1,2.1,3])
+print(tuple('1 23'))
+print(dict[1:2,3:4])
+print(set[1,2,3])
+
+a = 53
+b = 2.5
+
+print(a + b == 55.5)
+print(a + b == 54)
+print(a + b)
+print(a - b)
+print(a % b)
+print(a // b)
+print(a ** b)
+# комент тест 2.0
+"""
+комент тесь 2.0 але крутіший
+"""
+
+input("Ведіть щось: ")
+
+# test
+
+text_thingy = input("те саме але ще воно повториться5 разів: ")
+print(text_thingy * 5)
